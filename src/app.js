df97b1534d3abfe0df67b1df73bf0fd137234ca4
giulianac-git <79769030+giulianac-git@users.noreply.github.com>
2023-06-27 02:57:20 +0000

    Random Card Generator with Vanilla JS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,64 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+};
+
+// //Card is randomly generated with a timer
+// window.onload = () => {
+//   document.querySelector(".card").classList.add(generateRandomSuit());
+//   document.querySelector(".number").innerHTML = generateRandomNumber();
+//   setTimeout(() => {
+//     document.querySelector(".card").classList.add(generateRandomSuit());
+//     document.querySelector(".number").innerHTML = generateRandomNumber();
+//   }, 10000);
+// };
+
+// //Random card is generated on click
+// document.getElementById("btn").addEventListener("click", function() {
+//   document.querySelector(".card").classList.add(generateRandomSuit());
+//   document.querySelector(".number").innerHTML = generateRandomNumber();
+// });
+
+// //User specifies card dimensions with inputs
+// let widthField = document.querySelector("#widthValue");
+// let heightField = document.querySelector("#heightValue");
+// widthField.addEventListener("input", adjustWidth);
+// heightField.addEventListener("input", adjustHeight);
+// function adjustWidth() {
+//   let width = widthField.value;
+//   document.querySelector(".card").style.width = width + "px";
+// }
+// function adjustHeight() {
+//   let height = heightField.value;
+//   document.querySelector(".card").style.height = height + "px";
+// }
+
+let generateRandomSuit = () => {
+  let suit = ["heart", "spade", "club", "diamond"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumber];
 };
